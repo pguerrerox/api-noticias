0bff303fc95b86849698e808004c771e7a045060
Pedro Guerrero <pguerrerox@users.noreply.github.com>
2020-02-09 20:44:54 -0500

    scraping trigger logic 2....

--- scraper/aux/allScrapers.js
+++ scraper/aux/allScrapers.js
@@ -10,80 +10,19 @@ const puppeteer = require('puppeteer');
 // helpers
 const sites = require('./sites');
 
-// goScraper(), call the scraper_modules for the sites and params define on the sites.js helper...
-const goScraper = function (_sites) {
-  // scraper_modules
-  let scraperModule = require(`../scraper_modules/${site.site}`);
-  let baseUrl = site.baseUrl;
-  let param = site.param;
-  console.log(`Scraping...${site.site}-${param}`);
-  scraperModule(baseUrl, param);
-  // _sites.forEach(site => {
-  //   params.forEach(param => {
-  //   })
-  // });
-}
+fireEmAll(sites);
+// main function...
+async function fireEmAll(_sites){
+  console.log(`inicio...`);
 
-// headless/request
-async function puppeRequest(_site){
-  // let url = _site.site;
-  // console.log(url);
-  try{
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(`${_site.baseUrl}${_site.param}`);
-    await page.waitForSelector('body', {timeout: 2000});
-
-    const body = await page.evaluate(() => {
-      return document.querySelector('body').innerHTML;
-    });
-    // console.log(body);
-    await browser.close();
-    return body
-  }
-  catch(err){
-    console.log(err);
-  }
-}
-
-// comparator func
-async function compareFunc(_oldData, testNo, _site) {
-  try{
-    let newData = await puppeRequest(_site);
-
-    // difference calc
-    let diffx = diff.diffChars(_oldData, newData);
-    let total = diffx.filter((x) => !x.added && !x.removed).reduce((acc, obj) => (acc + obj.count), 0);
-    let changed = diffx.filter((x) => x.added == true || x.removed == true).reduce((acc, obj) => (acc + obj.count), 0);
-    let result = Math.round((changed/total)*100);
-    
-    // console.log(`TEST: ${testNo}`);
-    if (result > 25){
-      // console.log('scraped-now');
-      goScraper(_site);
-      _oldData = newData;
-    }
-    // console.log('do-not-scraped');
-    // console.log("===============");
-    testNo++;
-    return setTimeout(() => compareFunc(_oldData, testNo), 1000 * 15)
-  }
-  catch (err){
-    console.error(err);
-  }
-}
-
-// export
-async function final(_sites){
   try {
     _sites.forEach(site => {
       let params = site.param;
-
       params.forEach( async param => {
-        console.log(`Chequeando...${site.site}-${param}`)
-        let oldData = await puppeRequest(site);
         let test = 1;
-        return await compareFunc(oldData, test+1, site)
+        let baseUrl = site.baseUrl;
+        let oldData = await puppeRequest(baseUrl, param);
+        return compareFunc(oldData, test, site, baseUrl, param)
       })
     })
   }
@@ -91,6 +30,71 @@ async function final(_sites){
     console.log(err);
   }
 }
-// final(sites);
 
-module.exports = final;
+// comparator func
+async function compareFunc(_oldData, testNo, _site, _url, _param) {
+  console.log(`comparando...${_url}${_param}`);
+
+  try{
+    let newData = await puppeRequest(_url, _param);
+
+    // difference calc
+    let diffx = diff.diffChars(_oldData, newData);
+    let total = diffx.filter((x) => !x.added && !x.removed).reduce((acc, obj) => (acc + obj.count), 0);
+    let changed = diffx.filter((x) => x.added == true || x.removed == true).reduce((acc, obj) => (acc + obj.count), 0);
+    let result = Math.round((changed/total)*100);
+    
+    console.log(`TEST: ${testNo}`);
+    if (result > 25){
+      console.log(`llamando scraper para... ${_url}${_param}`);
+      goScraper(_site);
+      _oldData = newData;
+    }
+    else{
+      console.log(`no es necesario scrapear... ${_url}${_param}, tratar luego`);
+      console.log("===============");
+      testNo++;
+    }
+    return setTimeout(() => compareFunc(_oldData, testNo, _site, _url, _param), 1000 * 10)
+  }
+  catch (err){
+    console.error(err);
+  }
+}
+
+// headless/request
+async function puppeRequest(_url, _param){
+  console.log(`chequeando...${_url}${_param}`);
+
+  let fullUrl = String(_url+_param);
+  try{
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(fullUrl);
+    await page.waitForSelector('body', {timeout: 2000});
+
+    const body = await page.evaluate(() => {
+      return document.querySelector('body').innerHTML;
+    });
+    console.log(body.slice(0,1500));
+    await browser.close();
+    return body.slice(0,2000)
+  }
+  catch(err){
+    console.log(err);
+  }
+}
+
+// goScraper(), call the scraper_modules for the sites and params define on the sites.js helper...
+const goScraper = function (_site) {
+  console.log(`llamando scraper...${_site.baseUrl}${_site.param}`)
+  // scraper_modules
+  // let scraperModule = require(`../scraper_modules/${_site.site}`);
+  // let baseUrl = _site.baseUrl;
+  // let param = _site.param;
+  // console.log(`scraping...${baseUrl}${param}`);
+  // return scraperModule(baseUrl, param);
+}
+
+
+// module.exports = final;
--- scraper/aux/sites.js
+++ scraper/aux/sites.js
@@ -4,13 +4,7 @@ const sites = [
   {
     "site": "diariolibre",
     "baseUrl": "https://www.diariolibre.com/cronologia/ver/meta/",
-    "param": ["politica","deportes","cultura"],
-    "selector": "body"
-  },
-  {
-    "site": "listindiario",
-    "baseUrl": "https://www.listindiario.com/buscar?find=",
-    "param": ["politica","deportes","cultura"],
+    "param": ["politica"],
     "selector": "body"
   }
 ]
