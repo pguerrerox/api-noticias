116d561fcac0744d986ccaed2f60f36ce54cf4b9
Pedro Guerrero <pguerrerox@users.noreply.github.com>
2020-03-03 10:29:11 -0500

    saving to db kind of working...

--- scraper/aux/save-to-db.js
+++ scraper/aux/save-to-db.js
@@ -4,59 +4,60 @@
 
 // libraries
 const fs = require('fs');
+const path = require('path');
 
 // helpers
 const articuloModel = require('../models/article_model');
-// const sites = require('./aux/sites');
-// const testModel = require('./models/test_model');
+const testModel = require('../models/test_model');
 
 // mongoose
-// let mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true });
+let mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // database
-// let db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error'));
-// db.once('open', function () {
-//   console.log('Connected...')
-// });
-
-
-// FIXED LOOP THROUGH OBJECT>>>>
-
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function () {
+  console.log('Connected...')
+});
 
 // main function...
-// _sites: array of objects
 function saveToDB(_site, _param){
   console.log(`${_site}/${_param} - saving to DB`);
-  
-  // _sites.forEach(function (elem) {
-  //   let dataFile = JSON.parse(fs.readFileSync(`../../data/${elem.site}_${elem.params}.json`, 'utf8'));
-  //   for (let i = 0; i < dataFile.length; i++) {
-  //     let element = dataFile[i];
-  //     let articulo = new articuloModel({
-  //       link: element.link,
-  //       content: {
-  //         title: element.content.title,
-  //         author: element.content.author,
-  //         // date: element.content.date,
-  //         subcategory: element.content.subcategory,
-  //         summary: element.content.summary,
-  //         related_links: element.content.related_links
-  //       },
-  //       source: element.source,
-  //       category: element.category,
-  //       uuid: element.uuid,
-  //     })
-  //     articulo.save((err) => {
-  //       if (!err) {
-  //         console.log('done?')
-  //       } else {
-  //         console.error(err.message);
-  //       }
-  //     })
-  //   }
-  // })
+
+  let pathToJSON = path.join(__dirname, '..','..', 'data');
+  fs.readFile(`${pathToJSON}/${_site}_${_param}.json`, 'utf8', (err, data) => {
+    let dataFile = JSON.parse(data);
+    if(!err){
+      for (let i = 0; i < dataFile.length; i++) {
+        let element = dataFile[i];
+        let articulo = new articuloModel({
+        // let articulo = new testModel({
+          link: element.link,
+          content: {
+            title: element.content.title,
+            author: element.content.author,
+            date: element.content.date,
+            subcategory: element.content.subcategory,
+            summary: element.content.summary,
+            related_links: element.content.related_links
+          },
+          source: element.source,
+          category: element.category,
+          uuid: element.uuid,
+        });
+        articulo.save((err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      }
+      console.log(`Done with, ${_site}_${_param}`);
+    }
+    else {
+      throw err
+    }
+  })
 }
 
-module.exports = saveToDB;
+saveToDB('diariolibre', 'cultura');
